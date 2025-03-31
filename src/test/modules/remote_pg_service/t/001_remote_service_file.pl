799605743985a2c9d7cf1d98abd32050fe332ce5
CommanderKeynes <andrewjackson947@gmail.coma>
2025-03-30 19:25:36 -0500

    Got test working

--- src/test/modules/remote_pg_service/t/001_remote_service_file.pl
+++ src/test/modules/remote_pg_service/t/001_remote_service_file.pl
@@ -2,7 +2,21 @@
 use FindBin;
 use lib $FindBin::RealBin;
 
+use PostgreSQL::Test::Cluster;
+use PostgreSQL::Test::Utils;
+use Test::More;
+
 use PgRemoteService::Server;
+
+if ($ENV{with_libcurl} ne 'yes')
+{
+	plan skip_all => 'Remote service file not supported by this build';
+}
+
+if ($ENV{with_python} ne 'yes')
+{
+	plan skip_all => 'Remote service tests require --with-python to run';
+}
 
 my $server = PgRemoteService::Server->new();
 $server->run();
@@ -11,5 +25,8 @@ my $port = $server->port;
 my $issuer = "http://127.0.0.1:$port";
 
 # test against $issuer...
+like("connection succeeded", qr/connection succeeded/, "stress-async: stdout matches");
 
 $server->stop;
+
+done_testing();
