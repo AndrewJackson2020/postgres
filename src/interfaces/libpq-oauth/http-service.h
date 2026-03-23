64ec461648720e739f1bfdeced30539259bf9fb2
Andrew Jackson <ajackson@drwholdings.com>
2026-03-23 12:19:40 -0500

    Moved curl functionality to libpq-oauth. Compiles but tests fail

---
+++ src/interfaces/libpq-oauth/http-service.h
@@ -0,0 +1,29 @@
+/*-------------------------------------------------------------------------
+ *
+ * oauth-curl.
+ *
+ *	  Definitions for OAuth Device Authorization module
+ *
+ * Portions Copyright (c) 1996-2026, PostgreSQL Global Development Group
+ * Portions Copyright (c) 1994, Regents of the University of California
+ *
+ * src/interfaces/libpq-oauth/oauth-curl.h
+ *
+ *-------------------------------------------------------------------------
+ */
+
+#ifndef OAUTH_CURL_H
+#define OAUTH_CURL_H
+
+#include "libpq-fe.h"
+
+/* Exported flow callback. */
+extern PGDLLEXPORT int
+parse_service_file_curl(const char *serviceFile,
+					    const char *service,
+					    PQconninfoOption *options,
+					    PQExpBuffer errorMessage,
+					    bool *group_found);
+
+
+#endif							/* OAUTH_CURL_H */
